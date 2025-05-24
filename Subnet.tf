7e00e6e2c1ddf652261af295b106c08c38eeb02f
KarishmaAbruk <138876092+KarishmaAbruk@users.noreply.github.com>
2025-05-24 20:15:57 +0530

    Update Subnet.tf

--- Subnet.tf
+++ Subnet.tf
@@ -1,7 +1,7 @@
 
 resource "aws_subnet" "public_subnet" {
   vpc_id                  = aws_vpc.my_vpc.id
-  cidr_block              = "10.0.1.0/24"
+  cidr_block              = var.public_subnet_cidr
   map_public_ip_on_launch = "true"
   availability_zone       = "us-east-1a"
 
@@ -12,7 +12,7 @@ resource "aws_subnet" "public_subnet" {
 
 resource "aws_subnet" "private_subnet" {
   vpc_id            = aws_vpc.my_vpc.id
-  cidr_block        = "10.0.2.0/24"
+  cidr_block        = var.private_subnet_cidr
   availability_zone = "us-east-1b"
 
   tags = {
