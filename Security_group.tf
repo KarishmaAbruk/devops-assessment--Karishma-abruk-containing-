b991f5bf6f0527dc881b1e4026998d0c3d0b0fef
KarishmaAbruk <138876092+KarishmaAbruk@users.noreply.github.com>
2025-05-24 20:19:07 +0530

    Update Security_group.tf

--- Security_group.tf
+++ Security_group.tf
@@ -1,5 +1,5 @@
 resource "aws_security_group" "allow_http_ssh" {
-  name        = "allow-http-ssh"
+  name        = var.web_sg_name
   description = "Allow Ingress rules to allow SSH and HTTP connections"
   vpc_id      = aws_vpc.my_vpc.id
 }
